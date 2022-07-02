17d01d409d363b4c172062bdb72e56ae5f678515
Waleed1100x <107954669+Waleed1100x@users.noreply.github.com>
2022-07-02 18:08:41 +0300

    Task 2 Done

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function BMI(weight = 88,height = 1.73){
 }
 console.log(BMI(63,1.63));
 console.log(BMI());
+function Status(bmi){
+if(bmi <18.5){
+return "لديك نقص في الوزن"
+} 
+ if(bmi>=18.5 && bmi<25){
+return "وزنك صحي"
+}
+if(bmi>=25){
+return "لديك زيادة في الوزن"
+}
+
+}

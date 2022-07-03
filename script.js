35a42bd1b9155e9ae9361922b924bfb03cfb50e5
Waleed1100x <107954669+Waleed1100x@users.noreply.github.com>
2022-07-03 21:22:44 +0300

    Task 3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const btn1 = document.getElementById("Button");
 function BMI(weight = 88,height = 1.73){
     return(weight/(height*height));
 }
@@ -5,13 +6,19 @@ console.log(BMI(63,1.63));
 console.log(BMI());
 function Status(bmi){
 if(bmi <18.5){
-return "لديك نقص في الوزن"
+return 'لديك نقص في الوزن';
 } 
  if(bmi>=18.5 && bmi<25){
-return "وزنك صحي"
+return 'وزنك صحي';
 }
 if(bmi>=25){
-return "لديك زيادة في الوزن"
+return 'لديك زيادة في الوزن';
 }
-
+}
+function calculate(){
+    let weight =+  document.getElementById("weight").value;
+    let height =+ document.getElementById("height").value;
+    let bmi = BMI(weight,height);
+    let desc = Status(bmi);
+    document.getElementById("result").innerHTML = 'You are '+ desc +  'And your BMI is: '+ bmi;
 }
